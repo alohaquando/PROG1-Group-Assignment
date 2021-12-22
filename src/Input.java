c686f8d030d62c31065cbd94f04ae7a11bb2121e
aloha.quan <aloha.liamdo@gmail.com>
2021-12-22 18:39:56 -0500

    Added ability to select "Weeks before" or "Weeks after" while inputting date. This should make met all requirements of the input.

--- src/Input.java
+++ src/Input.java
@@ -66,18 +66,18 @@ public class Input {
     // Get date type
     static String inputDateType() {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("\nPick how to input date range. Write 'pair', 'days before', or 'days after': ");
+        System.out.print("\nPick how to input date range. Write 'pair', 'days before', 'days after', 'weeks before', 'weeks after': ");
         String inputDateType = scanner.nextLine();
 
         // Validate date type
         while (true) {
             switch (inputDateType) {
-                case "pair", "days before", "days after" -> {
+                case "pair", "days before", "days after", "weeks before", "weeks after" -> {
                     return inputDateType;
                 }
                 default -> {
                     System.out.println("Invalid input. Please try again.");
-                    System.out.print("\nPick how to input date range. Write 'pair', 'days before', or 'days after': ");
+                    System.out.print("\nPick how to input date range. Write 'pair', 'days before', 'days after', 'weeks before', 'weeks after': ");
                     inputDateType = scanner.nextLine();
                 }
             }
@@ -119,6 +119,7 @@ public class Input {
                     }
                 }
             }
+
             case "days before" -> {
                 System.out.print("\nNumbers of days before 1st date: ");
                 while (!scanner.hasNextInt()) {
@@ -130,6 +131,7 @@ public class Input {
                 LocalDate date2Formatted = result[0].minusDays(days);
                 result[1] = date2Formatted;
             }
+
             case "days after" -> {
                 System.out.print("\nNumbers of days after 1st date: ");
                 while (!scanner.hasNextInt()) {
@@ -139,6 +141,30 @@ public class Input {
                 }
                 int days = scanner.nextInt();
                 LocalDate date2Formatted = result[0].plusDays(days);
+                result[1] = date2Formatted;
+            }
+
+            case "weeks before" -> {
+                System.out.print("\nNumbers of weeks before 1st date: ");
+                while (!scanner.hasNextInt()) {
+                    System.out.println("Invalid input. Try again.");
+                    System.out.print("\nNumbers of weeks before 1st date: ");
+                    scanner.nextLine();
+                }
+                int weeks = scanner.nextInt();
+                LocalDate date2Formatted = result[0].minusWeeks(weeks);
+                result[1] = date2Formatted;
+            }
+
+            case "weeks after" -> {
+                System.out.print("\nNumbers of weeks after 1st date: ");
+                while (!scanner.hasNextInt()) {
+                    System.out.println("Invalid input. Try again.");
+                    System.out.print("\nNumbers of weeks after 1st date: ");
+                    scanner.nextLine();
+                }
+                int weeks = scanner.nextInt();
+                LocalDate date2Formatted = result[0].plusWeeks(weeks);
                 result[1] = date2Formatted;
             }
         }
@@ -189,16 +215,19 @@ public class Input {
                 return scanner.nextInt();
             }
             case "by days" -> {
-                System.out.print("\nNumber of days: ");
+                System.out.println("\nNumber of days selected: " + days);
+                System.out.print("Number of days SELECTED must be divisible by number of days to group. Number of days to group: ");
                 while (!scanner.hasNextInt()) {
                     System.out.println("Invalid input. Try again.");
-                    System.out.print("\nNumber of days: ");
+                    System.out.println("\nNumber of days selected: " + days);
+                    System.out.print("Number of days SELECTED must be divisible by number of days to group. Number of days to group: ");
                     scanner.nextLine();
                 }
                 int daysGroup = scanner.nextInt();
                 while (days % daysGroup != 0) {
                     System.out.println("Invalid input. Try again.");
-                    System.out.print("\nNumber of days: ");
+                    System.out.println("\nNumber of days selected: " + days);
+                    System.out.print("Number of days SELECTED must be divisible by number of days to group. Number of days to group: ");
                     scanner.nextLine();
                     daysGroup = scanner.nextInt();
                 }
