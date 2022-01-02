c66707819d58c6373d155ba8ef1a2909211442ef
SonCaos3916151 <86813958+SonCaos3916151@users.noreply.github.com>
2022-01-02 19:51:49 +0700

    User can now decide to keep using or quit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         // Process the CSV file to use throughout the function
         List<List<Object>> data = ProcessCSV.process();
 
-        // Get all needed input
-        Input.getAllInput(data);
-        new Display(data);
-
+        System.out.println("Welcome to our group project");
+        System.out.println("Type 'yes' if you want to continue using our program and 'no' if you want to quit");
+        String user_input = scanner.nextLine();
+        while (!user_input.equals("no")) {
+            // Get all needed input
+            Input.getAllInput(data);
+            new Display(data);
+            System.out.println("Type 'yes' if you want to continue using our program and 'no' if you want to quit");
+            user_input = scanner.nextLine();
+        }
+        System.out.println("Thank you for using our program!");
     }
 }
