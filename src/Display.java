d294294d4653871aa81b0bda4959dd98816f9fb8
SonCaos3916151 <86813958+SonCaos3916151@users.noreply.github.com>
2022-01-02 16:29:22 +0700

    Calculation part now work

--- src/Display.java
+++ src/Display.java
@@ -1,9 +1,8 @@
+package com.company;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
+
 import static com.company.Input.*;
 
 public class Display {
@@ -11,7 +10,9 @@ public class Display {
     Display(List<List<Object>> data){
         ProcessCSV processed_data = new ProcessCSV(); // call the processCSV class to take data
         if (tableType.equals("tabular")) {
-            System.out.println("Range\tValue");
+            System.out.println("---------------------------------------------------------------------------");
+            System.out.println("| Range\t\t\t\t\t\t\t |\tValue                                 |");
+            System.out.println("---------------------------------------------------------------------------");
             ArrayList<String> range_input_list = new ArrayList<>();
             List<LocalDate> date_pair_each_row = new ArrayList<>(); //include the start and end date of each group. If user select none then this will hold date of each group
             //Specify Groupings
@@ -19,8 +20,6 @@ public class Display {
 
             if (date_pair[0].isEqual(date_pair[1])) { //user input 1 date
                 date_pair_each_row.add(date_pair[0]);
-                System.out.println("Correct");
-                //range_input_list.add(date_pair[0]);
             }
             else { //user input 2 different dates
                 if (date_pair[0].isAfter(date_pair[1])) {
@@ -84,27 +83,27 @@ public class Display {
             //Convert list of range input to array and output it as range column
             String[] range_input = new String[range_input_list.size()];
             range_input = range_input_list.toArray(range_input);
-            System.out.println(Arrays.toString(range_input));
 
 
             int date_pair_index = 0;
-            //List<List<Object>> data_for_calculation = data_for_calculation
 
             //Input data for value columns. Loops through each rows date from Range collumn and print out the entire table
-            for (int rows_index = 0; rows_index < range_input.length; rows_index ++) { //for each row
+            for (String s : range_input) { //for each row
 
                 List<LocalDate> every_date_each_row = new ArrayList<>();
                 int sum = 0; //sum of data for each row/ reset after every row
+                //int imaginary_sum = 0; //sum of imaginary date to use if groupType = None or select only 1 date
                 LocalDate start_date_of_row = date_pair_each_row.get(date_pair_index);
                 LocalDate end_date_of_row;
-                List<List<Object>> chosen_metric_data = null;
+                List<List<Object>> chosen_metric_data;
+                List<List<Object>> total_chosen_metric_data = new ArrayList<>();
+
                 List<Integer> int_metric_data; //convert object list into integer list for calculation
 
                 if (date_pair[0].isEqual(date_pair[1]) || groupType.equals("none")) {
                     end_date_of_row = start_date_of_row; //end date of row is the same as start day or row. Means each row only 1 date
                     date_pair_index = date_pair_index + 1;
-                }
-                else {                     // if user input multiple date and each row have multiple days
+                } else {                     // if user input multiple date and each row have multiple days
                     end_date_of_row = date_pair_each_row.get(date_pair_index + 1);
                     date_pair_index = date_pair_index + 2;
                 }
@@ -113,66 +112,88 @@ public class Display {
                     //Choose between three metric
                     if (metricType.equals("vaccinated")) {
                         if (start_date_of_row.compareTo(end_date_of_row) == 0) { // equals if they are both the same
-                            chosen_metric_data = processed_data.find_data(data, location,start_date_of_row,2,3 );
-                            int_metric_data = chosen_metric_data.stream() //In case there's more than 1 data that have the same date. Convert object list into integer list
+                            chosen_metric_data = processed_data.find_data(data, location, start_date_of_row, 2, 3); //data of that date
+                            total_chosen_metric_data.addAll(chosen_metric_data); //add data from that date to another list that contains all data of date in row
+
+                            int_metric_data = total_chosen_metric_data.stream() //In case there's more than 1 data that have the same date. Convert object list into integer list
                                     .flatMap(Collection::stream)
-                                    .map(ob->(Integer)ob)
+                                    .map(ob -> (Integer) ob)
                                     .collect(Collectors.toList());
 
                             for (int i : int_metric_data) {
                                 sum += i;
                             }
+                            total_chosen_metric_data.clear(); //clear from start_date_of_row data still in total_chosen_metric_data list
                             LocalDate imaginary_date_before = start_date_of_row.minusDays(1); //create an imaginary date to minus.
-                            chosen_metric_data = processed_data.find_data(data, location,imaginary_date_before,2,3 );
-                            int_metric_data = chosen_metric_data.stream() //In case there's more than 1 data that have the same date. Convert object list into integer list
+                            chosen_metric_data = processed_data.find_data(data, location, imaginary_date_before, 2, 3);
+                            total_chosen_metric_data.addAll(chosen_metric_data); //add data from that date to another list that contains all data of date in row
+
+                            int_metric_data = total_chosen_metric_data.stream() //In case there's more than 1 data that have the same date. Convert object list into integer list
                                     .flatMap(Collection::stream)
-                                    .map(ob->(Integer)ob)
+                                    .map(ob -> (Integer) ob)
                                     .collect(Collectors.toList());
 
                             for (int i : int_metric_data) {
                                 sum = sum - i; //Sum is the sum of date above. We need to new date only from the date of that row. So we have to minus it with imaginary accumlated data from yesterday
                             }
-                        }
-                        else { // if more than 1 date for each row
-                            chosen_metric_data = processed_data.find_data(data, location,end_date_of_row,2,3 );
+                        } else { // if more than 1 date for each row
+                            chosen_metric_data = processed_data.find_data(data, location, end_date_of_row, 2, 3);
+                            total_chosen_metric_data.addAll(chosen_metric_data); //add data from that date to another list that contains all data of date in row
+
                             int_metric_data = chosen_metric_data.stream() //In case there's more than 1 data that have the same date, because it means there will be more than 1 element in the list. Convert object list into integer list
                                     .flatMap(Collection::stream)
-                                    .map(ob->(Integer)ob)
+                                    .map(ob -> (Integer) ob)
                                     .collect(Collectors.toList());
 
                             for (int i : int_metric_data) {
                                 sum += i;
                             }
-                            chosen_metric_data = processed_data.find_data(data, location,start_date_of_row,2,3 );
-                            int_metric_data = chosen_metric_data.stream() //In case there's more than 1 data that have the same date, because it means there will be more than 1 element in the list. Convert object list into integer list
+                            chosen_metric_data = processed_data.find_data(data, location, start_date_of_row, 2, 3);
+                            total_chosen_metric_data.addAll(chosen_metric_data); //add data from that date to another list that contains all data of date in row
+
+                            int_metric_data = total_chosen_metric_data.stream() //In case there's more than 1 data that have the same date, because it means there will be more than 1 element in the list. Convert object list into integer list
                                     .flatMap(Collection::stream)
-                                    .map(ob->(Integer)ob)
+                                    .map(ob -> (Integer) ob)
                                     .collect(Collectors.toList());
 
                             for (int i : int_metric_data) {
                                 sum = sum - i;
                             }
                         }
-                    }
-                    else { //else the other 2 metric. Since they have the same calculation method
+                    } else { //else the other 2 metric. Since they have the same calculation method
                         while (!start_date_of_row.isAfter(end_date_of_row)) {
                             every_date_each_row.add(start_date_of_row);
                             start_date_of_row = start_date_of_row.plusDays(1);
                         }
-                        if (metricType.equals("death")) {
-                            for (int i = 0; i < every_date_each_row.size(); i++) {
-                                chosen_metric_data = processed_data.find_data(data, location,every_date_each_row.get(i),2,3 );
-                            }
+                        for (LocalDate localDate : every_date_each_row) {
+                            chosen_metric_data = processed_data.find_data(data, location, localDate, 2, 3); //take data from that date
+                            total_chosen_metric_data.addAll(chosen_metric_data); //add data from that date to another list that contains all data of date in row
                         }
-                        else {
-                            for (int i = 0; i < every_date_each_row.size(); i++) {
-                                chosen_metric_data = processed_data.find_data(data, location,every_date_each_row.get(i),2,3 );
-                            }
-                        }
-
-                        int_metric_data = chosen_metric_data.stream()
+                        int_metric_data = total_chosen_metric_data.stream()
                                 .flatMap(Collection::stream)
-                                .map(ob->(Integer)ob)
+                                .map(ob -> (Integer) ob)
+                                .collect(Collectors.toList());
+
+                        for (int i : int_metric_data) {
+                            sum += i;
+                        }
+                    }
+                } else { //if it is up to
+                    if (metricType.equals("vaccinated")) {
+                        System.out.println("hello");
+                    } else {
+                        start_date_of_row = date_pair[0];
+                        while (!start_date_of_row.isAfter(end_date_of_row)) {
+                            every_date_each_row.add(start_date_of_row);
+                            start_date_of_row = start_date_of_row.plusDays(1);
+                        }
+                        for (LocalDate localDate : every_date_each_row) {
+                            chosen_metric_data = processed_data.find_data(data, location, localDate, 2, 3); //take data from that date
+                            total_chosen_metric_data.addAll(chosen_metric_data); //add data from that date to another list that contains all data of date in row
+                        }
+                        int_metric_data = total_chosen_metric_data.stream()
+                                .flatMap(Collection::stream)
+                                .map(ob -> (Integer) ob)
                                 .collect(Collectors.toList());
 
                         for (int i : int_metric_data) {
@@ -180,37 +201,15 @@ public class Display {
                         }
                     }
                 }
-                else { //if it is up to
-                    start_date_of_row = date_pair[0];
-                    while (!start_date_of_row.isAfter(end_date_of_row)) {
-                        every_date_each_row.add(start_date_of_row);
-                        start_date_of_row = start_date_of_row.plusDays(1);
-                    }
-                    //Choose between three metric
-                    if (metricType.equals("vaccinated")) {
-                        for (int i = 0; i < every_date_each_row.size(); i++) {
-                            chosen_metric_data = processed_data.find_data(data, location,every_date_each_row.get(i),2,3 );
-                        }
-                    } else if ((metricType.equals("death"))) {
-                        for (int i = 0; i < every_date_each_row.size(); i++) {
-                            chosen_metric_data = processed_data.find_data(data, location,every_date_each_row.get(i),2,3 );
-                        }
-                    } else { //new cases
-                        for (int i = 0; i < every_date_each_row.size(); i++) {
-                            chosen_metric_data = processed_data.find_data(data, location,every_date_each_row.get(i),2,3 );
-                        }
-                    }
-                    int_metric_data = chosen_metric_data.stream()
-                            .flatMap(Collection::stream)
-                            .map(ob->(Integer)ob)
-                            .collect(Collectors.toList());
-                    for (int i : int_metric_data) {
-                        sum += i;
-                    }
-                }
                 //Print out table with range column on the left and value column(sum) on the right
-                System.out.println(range_input[rows_index] + "\t" + sum);
+                if (date_pair[0].isEqual(date_pair[1]) || groupType.equals("none")) {
+                    System.out.println("  " + s + "\t\t\t\t\t | \t\t" + sum);
+                } else {
+                    System.out.println("  " + s + "\t\t | \t\t" + sum);
+                }
             }
+            System.out.println("---------------------------------------------------------------------------");
+
         }
     }
 }
